15c8d477c7aa3f945a79135bcfa7a3097a4729ca
kanchan.phutane <kanchanphutane10.kp@gmail.com>
2026-05-25 01:31:15 +0530

    updated

--- src/app/shop/home/logo/logo.component.ts
+++ src/app/shop/home/logo/logo.component.ts
@@ -52,7 +52,7 @@ export class LogoComponent implements OnInit {
 
   ngOnInit(): void {
     this._httpService.get(environment.BASE_API_PATH + "Brandlogo/GetAll").subscribe(res => {
-      res.data = "assets/images/logo 1.jpeg";
+      // res.data = "assets/images/logo 1.jpeg";
 
       this.logos = res.data;
     });
